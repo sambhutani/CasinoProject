2673872c67a708155455e23fd1319b2bdbc848b6
Lauren Green <laurengreen@zipcoders-MacBook-Pro-3.local>
2018-11-05 17:52:35 -0500

    Backing up Scorecard changes

--- src/main/java/io/zipcoder/casino/ScoreSheet.java
+++ src/main/java/io/zipcoder/casino/ScoreSheet.java
@@ -2,17 +2,42 @@
 
 package io.zipcoder.casino;
 
+import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ScoreSheet {
-    private Map<String, Integer> scores = new HashMap<>();
 
-    ScoreSheet(){}
-
-    public int getScore() {
-        return 0;
+    private enum ROW{
+        ACES, TWOS, THREES, FOURS, FIVES, SIXES, THREEOFAKIND, FOUROFAKIND, FULLHOUSE, SMALLSTRAIGHT, LARGESTRAIGHT, YAHTZEE, CHANCE;
     }
+    private Map<ROW, Integer> scores = new EnumMap<>(ROW.class);
+
+    ScoreSheet(){
+
+    }
+
+    public int getTotalScore() {
+
+        int topTotalScore = 0;
+        int bottomTotalScore = 0;
+        int index = 0;
+        for(ROW r : ROW.values()) {
+            while (index < 6) {
+                topTotalScore += scores.get(r);
+                index++;
+            }
+            if (topTotalScore >= 63) {
+                topTotalScore += 35;
+            }
+            while (index >= 6) {
+                bottomTotalScore += scores.get(r);
+                index++;
+            }
+        }
+        return topTotalScore + bottomTotalScore;
+        }
+
 
     public void setScore(int score) {
 
@@ -22,11 +47,40 @@ public class ScoreSheet {
 
     }
 
-    public void setRow(String setRow){
+    public void setRow(ROW row){
+
+        switch(row) {
+            case ACES:
+                break;
+            case TWOS:
+                break;
+            case THREES:
+                break;
+            case FOURS:
+                break;
+            case FIVES:
+                break;
+            case SIXES:
+                break;
+            case THREEOFAKIND:
+                break;
+            case FOUROFAKIND:
+                break;
+            case FULLHOUSE:
+                break;
+            case SMALLSTRAIGHT:
+                break;
+            case LARGESTRAIGHT:
+                break;
+            case YAHTZEE:
+                break;
+            case CHANCE:
+                break;
+        }
 
     }
 
-    public void calculateScore(String row, int value){
+    public void calculateScore(ROW row, int value){
 
     }
 }
