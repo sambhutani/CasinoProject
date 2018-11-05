fab1cf12f46c7501e27fc085b3034c72d2fa72c9
nicholassatinover <nsatinover@gmail.com>
2018-11-05 18:34:53 -0500

    Card and Deck class complete

---
+++ src/test/java/io/zipcoder/casino/CardTest.java
@@ -0,0 +1,20 @@
+package io.zipcoder.casino;
+
+import org.junit.Test;
+
+public class CardTest {
+
+    @Test
+    public void createCard(){
+        Card card = new Card(Card.CardValue.TWO, Card.Suit.CLUBS);
+        System.out.println(card.getName());
+    }
+
+    @Test
+    public void createCard2(){
+        Card card = new Card(Card.CardValue.TWO, Card.Suit.CLUBS);
+        Card card1 = new Card(Card.CardValue.QUEEN, Card.Suit.HEARTS);
+        System.out.println(card.getName());
+        System.out.println(card1.getName());
+    }
+}
