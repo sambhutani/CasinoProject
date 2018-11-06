a267ac345e46620a8266612544cb059cfd2fcbe5
Lauren Green <laurengreen@zipcoders-MacBook-Pro-3.local>
2018-11-05 20:17:26 -0500

    Most methods written for ScoreSheet

--- src/main/java/io/zipcoder/casino/ScoreSheet.java
+++ src/main/java/io/zipcoder/casino/ScoreSheet.java
@@ -2,9 +2,7 @@
 
 package io.zipcoder.casino;
 
-import java.util.EnumMap;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class ScoreSheet {
 
@@ -47,51 +45,125 @@ public class ScoreSheet {
 
     public void setRow(ROW row, Dice[] cup){
 
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(cup);
+        Collections.sort(numbers);
+
         switch(row) {
             case ACES:
-                scores.put(ROW.ACES, calculateScore(ROW.ACES, cup);
+                scores.put(ROW.ACES, scoreNumbers(numbers, 1));
                 break;
             case TWOS:
-                scores.put(ROW.TWOS, calculateScore(ROW.TWOS, cup);
+                scores.put(ROW.TWOS, scoreNumbers(numbers, 2));
                 break;
             case THREES:
-                scores.put(ROW.THREES, calculateScore(ROW.THREES, cup);
+                scores.put(ROW.THREES, scoreNumbers(numbers, 3));
                 break;
             case FOURS:
-                scores.put(ROW.FOURS, calculateScore(ROW.FOURS, cup);
+                scores.put(ROW.FOURS, scoreNumbers(numbers, 4));
                 break;
             case FIVES:
-                scores.put(ROW.FIVES, calculateScore(ROW.FIVES, cup);
+                scores.put(ROW.FIVES, scoreNumbers(numbers, 5));
                 break;
             case SIXES:
-                scores.put(ROW.SIXES, calculateScore(ROW.SIXES, cup);
+                scores.put(ROW.SIXES, scoreNumbers(numbers, 6));
                 break;
             case THREEOFAKIND:
-                scores.put(ROW.THREEOFAKIND, calculateScore(ROW.THREEOFAKIND, cup);
+                if(checkOfaKind(numbers, 3)) {
+                scores.put(ROW.THREEOFAKIND, scoreTotalDice(numbers));
+                } else {
+                    scores.put(ROW.THREEOFAKIND, 0); 
+                }
                 break;
             case FOUROFAKIND:
-                scores.put(ROW.FOUROFAKIND, calculateScore(ROW.FOUROFAKIND, cup);
+                if(checkOfaKind(numbers, 4)) {
+                    scores.put(ROW.FOUROFAKIND, scoreTotalDice(numbers));
+                } else {
+                    scores.put(ROW.FOUROFAKIND, 0);
+                }
                 break;
             case FULLHOUSE:
-                scores.put(ROW.FULLHOUSE, calculateScore(ROW.FULLHOUSE, cup);
+                if(checkOfaKind(numbers,5) || checkFullHouse(numbers)) {
+                    scores.put(ROW.FULLHOUSE, 25);
+                } else {
+                    scores.put(ROW.FULLHOUSE, 0);
+                }
                 break;
             case SMALLSTRAIGHT:
-                scores.put(ROW.SMALLSTRAIGHT, calculateScore(ROW.SMALLSTRAIGHT, cup);
+                if(checkSmallStraight()) {
+                    scores.put(ROW.SMALLSTRAIGHT, 30);
+                } else {
+                    scores.put(ROW.SMALLSTRAIGHT, 0);
+                }
                 break;
             case LARGESTRAIGHT:
-                scores.put(ROW.LARGESTRAIGHT, calculateScore(ROW.LARGESTRAIGHT, cup);
+                if(checkLargeStraight()) {
+                    scores.put(ROW.LARGESTRAIGHT, 40);
+                } else {
+                    scores.put(ROW.LARGESTRAIGHT, 0);
+                }
                 break;
             case YAHTZEE:
-                scores.put(ROW.YAHTZEE, calculateScore(ROW.YAHTZEE, cup);
+                if(checkOfaKind(numbers, 5)) {
+                    scores.put(ROW.YAHTZEE, 50);
+                } else {
+                    scores.put(ROW.YAHTZEE, 0);
+                }
                 break;
             case CHANCE:
-                scores.put(ROW.CHANCE, calculateScore(ROW.CHANCE, cup);
+                scores.put(ROW.CHANCE, scoreTotalDice(numbers));
                 break;
         }
 
     }
 
-    public void calculateScore(ROW row, Dice[] cup){
+    private boolean checkFullHouse(ArrayList<Integer> numbers) {
 
+        boolean check2 = checkOfaKind(numbers, 2);
+        boolean check3 = checkOfaKind(numbers, 3);
+
+        return (check2 && check3);
+    }
+
+    private boolean checkSmallStraight() {
+
+        return false;
+    }
+
+    private boolean checkLargeStraight() {
+
+        return false;
+    }
+
+    private boolean checkOfaKind(ArrayList<Integer> numbers, int numb) {
+
+        int[] counts = new int[6];
+
+        for (int i = 0; i < numbers.size(); i++)
+            counts[numbers.get(i) - 1]++;
+
+        for (int i: counts) {
+            if (i == numb) return true;
+        }
+        return false;
+    }
+
+    private int scoreNumbers(ArrayList<Integer> numbers, int numb){
+
+        int count = 0;
+        for(Integer i : numbers) {
+            if(i == numb) {
+                count++;
+            }
+        }
+        return count * numb;
+    }
+
+    private int scoreTotalDice(ArrayList<Integer> numbers) {
+
+        int score = 0;
+        for(Integer i : numbers) {
+            score += i;
+        }
+        return score;
     }
 }
